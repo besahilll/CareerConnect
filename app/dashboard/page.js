0a8c50ca41796da2aae5271fc804cb1ea58b50db
avnigoyal25 <500090996@stu.upes.ac.in>
2024-10-19 17:30:50 +0530

    navbar added

---
+++ app/dashboard/page.js
@@ -0,0 +1,11 @@
+import Navbar from "./_components/navbar/page";
+import InterviewButton from "./_components/interview/page";
+
+export default function Dashboard() {
+  return (
+    <div>
+      <Navbar/>
+      <InterviewButton/>
+    </div>
+  );
+}
